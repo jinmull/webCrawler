e9eb066f8e2ca9115db42496616465486910329c
khjoony <khjoony@gmail.com>
2019-07-18 17:58:31 +0900

    Edit donga_getter_requests

--- googleC/packages/donga_getter_requests.py
+++ googleC/packages/donga_getter_requests.py
@@ -4,9 +4,8 @@
 """
  
 import sys
+import requests
 from bs4 import BeautifulSoup
-import urllib.request
-from urllib.parse import quote
  
 TARGET_URL_BEFORE_PAGE_NUM = "http://news.donga.com/search?p="
 TARGET_URL_BEFORE_KEWORD = '&query='
@@ -17,15 +16,12 @@ TARGET_URL_REST = '&check_news=1&more=1&sorting=3&search_date=1&v1=&v2=&range=3'
 def get_link_from_news_title(page_num, URL, output_file):
     for i in range(page_num):
         current_page_num = 1 + i*15
-        print('CURRENT_PAGE_NUM = ', current_page_num)
         position = URL.index('=')
-        print('POSITION = ',position)
         URL_with_page_num = URL[: position+1] + str(current_page_num) \
                             + URL[position+1 :]
-        print('URL_WITH_PAGE_NUM = ', URL_with_page_num)
-        source_code_from_URL = urllib.request.urlopen(URL_with_page_num)
-        soup = BeautifulSoup(source_code_from_URL, 'html.parser',
-                             from_encoding='utf-8')
+        source_code_from_URL = requests.get(URL_with_page_num).text
+        soup = BeautifulSoup(source_code_from_URL, 'html.parser', -1,
+                             encoding='utf-8')
         for title in soup.find_all('p', 'tit'):
             title_link = title.select('a')
             article_URL = title_link[0]['href']
@@ -34,8 +30,8 @@ def get_link_from_news_title(page_num, URL, output_file):
  
 # 기사 본문 내용 긁어오기 (위 함수 내부에서 기사 본문 주소 받아 사용되는 함수)
 def get_text(URL, output_file):
-    source_code_from_url = urllib.request.urlopen(URL)
-    soup = BeautifulSoup(source_code_from_url, 'html.parser', from_encoding='utf-8')
+    source_code_from_url = requests.get(URL).text
+    soup = BeautifulSoup(source_code_from_url, 'html.parser', -1, encoding='utf-8')
     content_of_article = soup.select('div.article_txt')
     for item in content_of_article:
         string_item = str(item.find_all(text=True))
@@ -56,8 +52,8 @@ def main(argv):
     """
 
     target_URL = TARGET_URL_BEFORE_PAGE_NUM + TARGET_URL_BEFORE_KEWORD \
-                 + quote(keyword) + TARGET_URL_REST
-    ## print('TARGET_URL', target_URL)
+                 + keyword + TARGET_URL_REST
+    print('TARGET_URL', target_URL)
     output_file = open(output_file_name, 'w')
     get_link_from_news_title(page_num, target_URL, output_file)
     output_file.close()
